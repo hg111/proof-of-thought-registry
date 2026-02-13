a486cfbac93f1d3ad4a49b0c7822a16e974bbbb3
hg111 <haggai@hg.local>
2026-02-12 16:46:34 -0800

    fix: set FeatureLock to position:fixed for mobile viewport centering

--- src/components/FeatureLock.tsx
+++ src/components/FeatureLock.tsx
@@ -44,7 +44,7 @@ export default function FeatureLock({
 
     return (
         <div style={{
-            position: "absolute",
+            position: "fixed",
             top: 0,
             left: 0,
             right: 0,
